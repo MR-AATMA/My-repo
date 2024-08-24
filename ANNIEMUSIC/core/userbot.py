fd87c15e6dabdba03802dcb1da3059f0ddcc6d18
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-08-24 11:35:49 +0530

    Update userbot.py

--- ANNIEMUSIC/core/userbot.py
+++ ANNIEMUSIC/core/userbot.py
@@ -53,7 +53,7 @@ class Userbot(Client):
             try:
                 await self.one.join_chat("BROKENXNETWORK")
               
-                await self.one.join_chat("BROKNXSUPPORT")
+                await self.one.join_chat("ABOUTBROKENX")
             except:
                 pass
             assistants.append(1)
@@ -76,7 +76,7 @@ class Userbot(Client):
             try:
                 await self.two.join_chat("BROKENXNETWORK")
                 
-                await self.one.join_chat("BROKNXSUPPORT")
+                await self.one.join_chat("ABOUTBROKENX")
             except:
                 pass
             assistants.append(2)
@@ -98,7 +98,7 @@ class Userbot(Client):
             try:
                 await self.three.join_chat("BROKENXNETWORK")
                 
-                await self.one.join_chat("BROKNXSUPPORT")
+                await self.one.join_chat("ABOUTBROKENX")
             except:
                 pass
             assistants.append(3)
@@ -120,7 +120,7 @@ class Userbot(Client):
             try:
                 await self.four.join_chat("BROKENXNETWORK")
                 
-                await self.one.join_chat("BROKNXSUPPORT")
+                await self.one.join_chat("ABOUTBROKENX")
             except:
                 pass
             assistants.append(4)
@@ -142,7 +142,7 @@ class Userbot(Client):
             try:
                 await self.five.join_chat("BROKENXNETWORK")
                 
-                await self.one.join_chat("BROKNXSUPPORT")
+                await self.one.join_chat("ABOUTBROKENX")
             except:
                 pass
             assistants.append(5)
